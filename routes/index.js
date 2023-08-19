d38aabe1b8957628d4298f99213a58d88e1104cb
pharpharlin <124903718+pharpharlin@users.noreply.github.com>
2023-08-19 15:40:22 +0630

    add user info

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/user/info', function(req, res, next) {
+  res.render('user_info');
+});
+
+router.get('/trip', function(req, res, next) {
+  res.render('trip');
 });
 
 module.exports = router;
