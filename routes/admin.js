caa29dfa9edf9ec1554e210a1579246298df03d5
pharpharlin <124903718+pharpharlin@users.noreply.github.com>
2023-08-20 12:52:07 +0630

    add new database

--- routes/admin.js
+++ routes/admin.js
@@ -6,4 +6,12 @@ router.get('/admin/travel/route', function(req, res, next) {
   res.render('admin/travel_route');
 });
 
+/* Get user lists. */
+router.get('/admin/user/list', function(req, res, next) {
+  res.render('admin/user_list');  
+});
+
+
+
+
 module.exports = router;
