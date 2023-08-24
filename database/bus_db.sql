693101f69fb311a4d6dced8fbdfe690ad229ef54
Oxy Thu <oxythu27@gmail.com>
2023-08-24 19:56:45 +0630

    db add

--- database/bus_db.sql
+++ database/bus_db.sql
@@ -1,10 +1,10 @@
 CREATE DATABASE  IF NOT EXISTS `bus` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `bus`;
--- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
 --
 -- Host: localhost    Database: bus
 -- ------------------------------------------------------
--- Server version	8.0.34
+-- Server version	8.0.28
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -100,6 +100,32 @@ INSERT INTO `route_table` VALUES (1,'Sittwe','Yangon','50000','05:00:00','2023-0
 UNLOCK TABLES;
 
 --
+-- Table structure for table `taken_seat_table`
+--
+
+DROP TABLE IF EXISTS `taken_seat_table`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `taken_seat_table` (
+  `taken_seat_id` int NOT NULL AUTO_INCREMENT,
+  `route_id` int DEFAULT NULL,
+  `user_id` int DEFAULT NULL,
+  `seat_no` int DEFAULT NULL,
+  PRIMARY KEY (`taken_seat_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=50 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `taken_seat_table`
+--
+
+LOCK TABLES `taken_seat_table` WRITE;
+/*!40000 ALTER TABLE `taken_seat_table` DISABLE KEYS */;
+INSERT INTO `taken_seat_table` VALUES (1,1,1,1),(2,1,1,10),(3,1,1,6),(4,1,1,9),(5,1,2,1),(6,1,2,10),(7,1,2,6),(8,1,2,9),(9,1,3,1),(10,1,3,4),(11,1,4,1),(12,1,4,2),(13,1,5,1),(14,1,5,4),(15,1,6,31),(16,1,6,32),(17,1,7,2),(18,1,8,2),(19,1,9,2),(20,1,10,3),(21,1,10,5),(22,1,10,7),(23,1,10,8),(24,1,11,3),(25,1,11,5),(26,1,11,8),(27,1,11,7),(28,1,18,11),(29,1,18,12),(30,1,19,15),(31,1,19,16),(32,1,20,13),(33,1,20,14),(34,1,21,17),(35,1,21,18),(36,1,22,19),(37,1,23,20),(38,1,24,23),(39,1,24,24),(40,1,25,27),(41,1,25,28),(42,1,26,22),(43,1,26,21),(44,1,29,25),(45,1,29,26),(46,1,30,29),(47,1,31,30),(48,1,32,33),(49,1,32,34);
+/*!40000 ALTER TABLE `taken_seat_table` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `ticket_table`
 --
 
@@ -114,7 +140,7 @@ CREATE TABLE `ticket_table` (
   `booking_date` datetime DEFAULT NULL,
   `status` int DEFAULT NULL,
   PRIMARY KEY (`ticket_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -123,7 +149,7 @@ CREATE TABLE `ticket_table` (
 
 LOCK TABLES `ticket_table` WRITE;
 /*!40000 ALTER TABLE `ticket_table` DISABLE KEYS */;
-INSERT INTO `ticket_table` VALUES (13,NULL,23,29,'2023-08-21 17:54:53',1),(14,NULL,24,13,'2023-08-22 11:45:51',1),(15,NULL,25,13,'2023-08-22 11:50:56',1),(16,NULL,26,13,'2023-08-22 12:04:44',0),(17,NULL,27,5,'2023-08-23 15:22:27',0);
+INSERT INTO `ticket_table` VALUES (1,NULL,1,1,'2023-08-24 00:34:32',0),(2,NULL,2,1,'2023-08-24 00:38:36',0),(3,NULL,3,1,'2023-08-24 00:38:37',0),(4,NULL,4,1,'2023-08-24 00:38:43',0),(5,NULL,5,1,'2023-08-24 00:39:00',0),(6,NULL,6,1,'2023-08-24 00:40:25',0),(7,NULL,7,1,'2023-08-24 00:40:53',0),(8,NULL,8,1,'2023-08-24 00:41:37',0),(9,NULL,9,1,'2023-08-24 00:41:52',0),(10,NULL,10,1,'2023-08-24 02:41:00',0),(11,NULL,11,1,'2023-08-24 02:41:08',0),(12,NULL,18,1,'2023-08-24 03:14:17',0),(13,NULL,19,1,'2023-08-24 03:16:35',0),(14,NULL,20,1,'2023-08-24 03:18:03',0),(15,NULL,21,1,'2023-08-24 03:19:05',0),(16,NULL,22,1,'2023-08-24 03:21:40',0),(17,NULL,23,1,'2023-08-24 03:22:57',0),(18,NULL,24,1,'2023-08-24 03:26:19',0),(20,NULL,26,1,'2023-08-24 03:27:57',0),(21,NULL,27,1,'2023-08-24 03:33:47',0),(22,NULL,28,1,'2023-08-24 03:34:40',0),(23,NULL,29,1,'2023-08-24 03:35:12',0),(24,NULL,30,1,'2023-08-24 03:40:42',0),(25,NULL,31,1,'2023-08-24 03:42:20',0),(26,NULL,32,1,'2023-08-24 17:05:06',0);
 /*!40000 ALTER TABLE `ticket_table` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -143,7 +169,7 @@ CREATE TABLE `user_table` (
   `address` varchar(45) DEFAULT NULL,
   `nrc` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -152,7 +178,7 @@ CREATE TABLE `user_table` (
 
 LOCK TABLES `user_table` WRITE;
 /*!40000 ALTER TABLE `user_table` DISABLE KEYS */;
-INSERT INTO `user_table` VALUES (1,'AST','male','oxy@gmaf.com','989898988','askdf','88sfasdf'),(2,'AST','male','oxy@gmaf.com','989898988','askdf','88sfasdf'),(3,'Aafdsa','male','asdf@alkdf.com','34823424','adskfasd','kasdf'),(4,'3rrr','female','r3ff','ref','fff','wdwd'),(5,'AST','male','minnsoehlainglksda','989898988','ffu','88sfasdf'),(6,'AST','male','minnsoehlainglksda','989898988','ffu','88sfasdf'),(7,'Mark','male','oxy@gmaf.com','222','A','111'),(8,'Aung Hein Zay','male','oxy@gmaf.com','989898988','askdf','88sfasdf'),(9,'Aung Hein Zay','male','adf@gmail.com','29324354','akdjfks','asdfas'),(10,'adf','female','oxy@gmaf.com','989898988','ffu','88sfasdf'),(11,'Aung Hein Zay','female','adf@gmail.com','12342134','asdsfad','34af'),(12,'Lwon Myint','male','afd@gmail.com','989898988','FFU','88sfasdf'),(13,'Mon soe lin','male','fuf','989898988','askdf','88sfasdf'),(14,'Tun Kyaw Soe','male','afy@gmaf.com','989898988','ffu','88sfasdf'),(15,'Pyay Ti Oo','male','adf@m.com','989898988','ffu','88sfasdf'),(16,'Ko Ko','male','minnsoehlainglksda','fyyu','ffu','88sfasdf'),(17,'Lwin Myint','male','lwonmrunt7879@gmail.com','09.....','Sittwe','11/panata....'),(18,'Mark','male','minnsoehlaing192001@gmail.com','09787543353','Sittwe ','6366373'),(19,'Ko aung','male','ahz007aunghz@gmail.com','087799','Dhsjjffj','Dhsus'),(20,'adf','male','soe@gmail.com OR \'a\'=\'a\'','9678','adt','1'),(21,'aa','male','dd','xx','aa','cc'),(22,'ass','male','ddyttyyu','989898988','dytd7r7s566','s5667'),(23,'Lwin Myint','male','lwonmrunt7879@gmail.com','09690676509','Sittwe','11/Pa Na Ta (Naing)-----'),(24,'dty','male','fuygyi','989898988','gyuggu','wdwd'),(25,'tfty','male','tyft7','00000','t7t7tff67','xtrdy'),(26,'FYUGIUI','male','RR67T788','000','TRFTU','DT'),(27,'Aung Hein Zay','male','admin@gmail.com','989898988','ffu','88sfasdf');
+INSERT INTO `user_table` VALUES (1,'adf','female','dsf@gmail.com','09111111111','sittway','ndf33/'),(2,'adf','female','dsf@gmail.com','09111111111','sittway','ndf33/'),(3,'Minn Soe Hlaing ','male','minnsoehlaing192001@gmail.com','09787543353','Sittwe','11/SATANA(N)132212'),(4,'Lwin Myint','male','lwonmrunt7879@gmail.com','09690676509','Sittwe','11/panata....'),(5,'Minn Soe Hlaing ','male','minnsoehlaing192001@gmail.com','09787543353','Sittwe','11/SATANA(N)132212'),(6,'Lwin Myint','male','lwonmrunt7879@gmail.com','09690676509','Sittwe','11/panata....'),(7,'sir aung','male','ahz007aunghz@gmail.com','09898495517','Kandawgyi','Akdjkfiwkx'),(8,'Toni','male','aungmyooo264901561@gmail.com','09264789','Sittwr','11/SAT(N)1234'),(9,'Toni','male','aungmyooo264901561@gmail.com','09264789','Sittwr','11/SAT(N)1234'),(10,'Mark','male','mornsoehlaing19@gmail.com','09787543353','Sittwe ','11/SATANA 132212'),(11,'Kk','male','tt@gmail.com','0987878676','Address','11/gaga'),(12,'Jj','male','ysy@Hdh.com','636363636','Gsgsg','11/sdf'),(13,'Jj','male','ysy@Hdh.com','636363636','Gsgsg','11/sdf'),(14,'Jj','male','ysy@Hdh.com','636363636','Gsgsg','11/sdf'),(15,'Jj','male','ysy@Hdh.com','636363636','Gsgsg','11/sdf'),(16,'Jj','male','ysy@Hdh.com','636363636','Gsgsg','11/sdf'),(17,'Jj','male','ysy@Hdh.com','636363636','Gsgsg','11/sdf'),(18,'Jj','male','ysy@Hdh.com','636363636','Gsgsg','11/sdf'),(19,'Tt','female','tt@gmail.com','09428634653','sittway','dfsdf'),(20,'Ko Ko','other','tt@gmail.com','09989898989','Tt','Yy'),(21,'daf','other','asfdasdf@gmail.com','09989898989','kdfj','dfsdf'),(22,'adf','male','soe@gmail.com','09111111111','sittway','ndf33/'),(23,'adf','male','Yu@mail.com','9387423874','dkfskdf','djfdf'),(24,'adf','male','soe@gmail.com','09111111111','adt','sdakf'),(25,'A','female','soe@gmail.com','09111111111','adt','11/sdf'),(26,'adf','female','soe@gmail.com','09111111111','sittway','sdakf'),(27,'adf','male','sdf7@gmail.com','09111111111','adt','11/sdf'),(28,'adf','male','df@sdf.com','423434234','sdfjaf','dfsdf'),(29,'A','male','sdf109@gmail.com','09111111111','adt','sdakf'),(30,'adf','male','soe@gmail.com','09111111111','sittway','sadfsd'),(31,'A','male','soe@gmail.com','09111111111','adt','1'),(32,'adf','male','soe@gmail.com','21341234231','asdfasd','dfsdf');
 /*!40000 ALTER TABLE `user_table` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -165,4 +191,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-08-23 20:36:07
+-- Dump completed on 2023-08-24 19:56:28
--- model/db_connection.js
+++ model/db_connection.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2')
 // create the connection to database
 exports.connection = mysql.createConnection({
     host: 'localhost',
-    user: 'admin',
+    user: 'root',
     password:'root',
     database: 'bus'
   });
